34846d200e5881ccb261482f1654677f60fbf3eb
@dank1910 <53305399+ewigspace1910@users.noreply.github.com>
2022-07-22 11:42:20 +0700

    new utils modules

--- modules/features_extraction/__init__.py
+++ modules/features_extraction/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 
 #dataset
-from models import create as create_model
+from .models import create as create_model
 from .cnn import extract_cnn_feature, extract_features
 from .database import FeatureDatabase
 
